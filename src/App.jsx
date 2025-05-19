2cd7ca2f597da6e766ac3073cd05cb21ae6a7976
Linus <linusigelstrom@gmail.com>
2025-05-19 18:51:14 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <p>Hello Fnörsk</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
