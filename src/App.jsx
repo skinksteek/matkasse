29ed32f3dd4ba37fe851d04dc5ee7f67440a86ad
Linus Igelström <linusigelstrom@gmail.com>
2025-07-10 19:46:10 +0200

    total color theme change and minor styling changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import SearchProducts from "./components/SearchProducts";
+import logo from "./assets/images/Matkasselogo.png";
 
 function App() {
   return (
-    <>
-      <div>
-        <div className="header">
-          <a href="/">
-            <h1>Matkasse</h1>
-          </a>
-        </div>
-        <SearchProducts />
+    <div>
+      <div className="header">
+        <a href="/">
+          <img className="logo" src={logo} alt="Matkasse Logo" />
+        </a>
       </div>
-    </>
+      <SearchProducts />
+    </div>
   );
 }
 
