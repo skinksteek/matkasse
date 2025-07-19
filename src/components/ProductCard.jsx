1f02e88a59362e8fc67c0380a18a9caf3a1056a4
Linus Igelström <linusigelstrom@gmail.com>
2025-07-19 21:36:56 +0200

    line break for compareOrdinaryPrice and style

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,5 +1,8 @@
 export const ProductCard = ({ product }) => {
   const storeClass = product.store.toLowerCase().replace(/\s+/g, "-");
+  {
+    console.log("CITYGROSS PRODUKT:", product);
+  }
   return (
     <>
       {product.imageURL && (
@@ -10,12 +13,29 @@ export const ProductCard = ({ product }) => {
         />
       )}
       <h2 className="product-name">{product.name}</h2>
-      <h3 className="product-price">
-        {product.price}kr - {product.volume}
-      </h3>
+      <div className="product-values">
+        <h3 className="product-price">
+          {product.price ? (
+            <>
+              {product.priceMultipleItems && `${product.priceMultipleItems} `}
+              {product.price}
+              <h4 className="product-volume">{product.volume}</h4>
+            </>
+          ) : (
+            <span className="product-no-price">No price available</span>
+          )}
+        </h3>
+      </div>
       <div className="product-price-comparison">
-        <span>{product.getMorePrice} - </span>
-        <span>{product.compareOrdinaryPrice}</span>
+        <p>{product.getMorePrice}</p>
+        <p>
+          {product.compareOrdinaryPrice.split("\n").map((line, index) => (
+            <span key={index}>
+              {line}
+              <br />
+            </span>
+          ))}
+        </p>
       </div>
 
       <footer className={`product-store ${storeClass}`}>{product.store}</footer>
