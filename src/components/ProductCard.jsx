e6e86b2e02229d61f699795c0398d4e1ac507087
Linus Igelström <linusigelstrom@gmail.com>
2025-07-20 17:30:46 +0200

    changed component structure, moved styling and styling update

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,44 +1,41 @@
 export const ProductCard = ({ product }) => {
   const storeClass = product.store.toLowerCase().replace(/\s+/g, "-");
-  {
-    console.log("CITYGROSS PRODUKT:", product);
-  }
+
   return (
     <>
-      {product.imageURL && (
-        <img
-          className="product-image"
-          src={product.imageURL}
-          alt={product.name}
-        />
-      )}
-      <h2 className="product-name">{product.name}</h2>
-      <div className="product-values">
-        <h3 className="product-price">
-          {product.price ? (
-            <>
-              {product.priceMultipleItems && `${product.priceMultipleItems} `}
-              {product.price}
-              <h4 className="product-volume">{product.volume}</h4>
-            </>
-          ) : (
-            <span className="product-no-price">No price available</span>
-          )}
-        </h3>
-      </div>
-      <div className="product-price-comparison">
-        <p>{product.getMorePrice}</p>
-        <p>
-          {product.compareOrdinaryPrice.split("\n").map((line, index) => (
-            <span key={index}>
-              {line}
-              <br />
-            </span>
-          ))}
-        </p>
-      </div>
+      <article className="product-card">
+        {product.imageURL && (
+          <img
+            className="product-image"
+            src={product.imageURL}
+            alt={product.name}
+          />
+        )}
 
-      <footer className={`product-store ${storeClass}`}>{product.store}</footer>
+        <h2 className="product-name">{product.name}</h2>
+
+        <div className="product-price-volume">
+          <p className="product-price">
+            {product.priceMultipleItems && `${product.priceMultipleItems} `}
+            {product.price}
+          </p>
+          <p className="product-volume">{product.volume}</p>
+        </div>
+        <div className="product-offer">
+          <p>{product.getMorePrice}</p>
+          <p>
+            {product.compareOrdinaryPrice.split("\n").map((line, index) => (
+              <span key={index}>
+                {line}
+                <br />
+              </span>
+            ))}
+          </p>
+        </div>
+        <footer className={`product-store ${storeClass}`}>
+          {product.store}
+        </footer>
+      </article>
     </>
   );
 };
